a7bf3cb46477839953f4481c4a088803737b5f2a
落叶归根 <1213786160@qq.com>
2017-04-23 18:31:29 +0800

    修改命令接口为内部类

--- src/castle/Game.java
+++ src/castle/Game.java
@@ -9,33 +9,68 @@ public class Game {
         
     public Game() 
     {
-//    	handlers.put("go", new HandlerGo());
-    	handlers.put("bye", new HandlerBye(this));
-    	handlers.put("help", new HandlerHelp(this));
-    	handlers.put("go", new HandlerGo(this));
+		handlers.put("go", new Handler() {
+			@Override
+			public boolean isBye() {return false;}
+			@Override
+			public void doCmd(String word) {
+				goRoom(word);
+			}
+		});
+		handlers.put("help", new Handler() {			
+			@Override
+			public boolean isBye() {return false;}			
+			@Override
+			public void doCmd(String word) {
+				System.out.println();
+				System.out.println("迷路了吗？你可以做的命令有：");
+				System.out.println("go   —— 前往下个地点。如：go east");
+				System.out.println("help —— 获取帮助");
+		        System.out.println("bye  —— 结束游戏");
+			}
+		});
+		handlers.put("bye", new Handler() {
+			@Override
+			public boolean isBye() {return true;}		
+			@Override
+			public void doCmd(String word) {
+		        System.out.println("感谢您的光临。再见！");
+		    }
+		});
         createRooms();
     }
 
     private void createRooms()
     {
-        Room outside, lobby, pub, study, bedroom;
-      
         //	制造房间
-        outside = new Room("城堡外");
-        lobby = new Room("大堂");
-        pub = new Room("小酒吧");
-        study = new Room("书房");
-        bedroom = new Room("卧室");
+		Room outside = new Room("城堡外");
+		Room lobby = new Room("大堂");
+		Room pub = new Room("小酒吧");
+		Room reception = new Room("会客厅");
+		Room garden = new Room("花园");
+		Room hall = new Room("大厅");
+		Room bedroom = new Room("卧室");
+		Room odeum = new Room("音乐厅");
         
         //	初始化房间的出口
-        outside.setExit("east", lobby);
-        outside.setExit("south", study);
-        outside.setExit("west", pub);
-        lobby.setExit("west", outside);
-        pub.setExit("east", outside);
-        study.setExit("north", outside);
-        study.setExit("east", bedroom);
-        bedroom.setExit("east", study);
+		outside.setExit("north", lobby);
+		lobby.setExit("north", garden);
+		lobby.setExit("east", pub);
+		lobby.setExit("south", outside);
+		lobby.setExit("west", reception);
+		pub.setExit("west", lobby);
+		pub.setExit("north", garden);
+		reception.setExit("east", lobby);
+		reception.setExit("north", garden);
+		garden.setExit("south", lobby);
+		garden.setExit("east", pub);
+		garden.setExit("west", reception);
+		lobby.setExit("up", hall);
+		hall.setExit("down", lobby);
+		hall.setExit("west", bedroom);
+		hall.setExit("east", odeum);
+		bedroom.setExit("east", hall);
+		odeum.setExit("west", hall);
 
         currentRoom = outside;  //	从城堡门外开始
     }
@@ -47,6 +82,11 @@ public class Game {
         System.out.println("如果需要帮助，请输入 'help' 。");
         System.out.println();
         showPrompt();
+    }
+    
+    public void run() {
+    	printWelcome();
+		play();
     }
 
     // 以下为用户命令
@@ -85,23 +125,13 @@ public class Game {
     			if( handler.isBye())
     				break;
     		}
-//    		if ( words[0].equals("help") ) {
-//    			printHelp();
-//    		} else if (words[0].equals("go") ) {
-//    			goRoom(words[1]);
-//    		} else if ( words[0].equals("bye") ) {
-//    			break;
-//    		}
 		}
         in.close();
     }
 	
 	public static void main(String[] args) {
 		Game game = new Game();
-		game.printWelcome();
-		game.play();
-        
-        System.out.println("感谢您的光临。再见！");
+		game.run();
 	}
 
 }
